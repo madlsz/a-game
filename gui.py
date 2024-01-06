47c022a0533410091865df742f5056e91f863f67
madlsz <dlugosz00@gmail.com>
2024-01-06 01:25:47 +0100

    python is strange

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 import pygame
-import random
+import copy
+
 import tetrominos
+import tile
 
 pygame.init()
 
@@ -9,80 +11,16 @@ GAME_HEIGHT = 1000*0.8
 
 FPS = 30
 
-class Entity():
-    def __init__(self,screen, tetromino):
-        self.screen = screen
-        self.tiles = [Tile(screen, 0,0,tetromino.color), Tile(screen, screen.get_width()/10,0,tetromino.color)]
-        self.tetromino = tetromino
-
-    def move_x(self, x):
-        for tile in self.tiles:
-            if not (tile.x + tile.width + x <= self.screen.get_width() and tile.x + x >= 0):
-                return
-        for tile in self.tiles:
-            tile.move_x(x)
-
-
-
-class Tile():
-    pk = 0
-    def __init__(self, screen, x, y, color = None):
-        self.pk = Tile.pk
-        Tile.pk += 1
-        self.screen = screen
-        self.rect = pygame.Rect(x,y,screen.get_width()/10,screen.get_height()/20)
-        if color is None:
-            self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        else:
-            self.color = color
-    
-    def draw(self):
-        pygame.draw.rect(self.screen, self.color, self.rect)
-        
-    def move(self, x, y):
-        self.rect.x += x
-        self.rect.y += y
-
-    def move_x(self, x):
-        if self.rect.x + self.rect.width + x <= self.screen.get_width() and self.rect.x + x >= 0:
-            self.rect.x += x
-
-    def move_y(self, y):
-        if self.rect.y + self.rect.height + y <= self.screen.get_height() and self.rect.y + y >= 0:
-            self.rect.y += y
-
-    @property
-    def x(self):
-        return self.rect.x
-    
-    @property
-    def y(self):
-        return self.rect.y
-    
-    # @y.setter
-    # def y(self, new_y):
-    #     self.rect.y = new_y
-    
-    @property
-    def width(self):
-        return self.rect.width
-    
-    @property
-    def height(self):
-        return self.rect.height
-    
-    # def at_bottom(self):
-    #     return False
-
-
 # Set up the drawing window
 screen = pygame.display.set_mode([GAME_WIDTH, GAME_HEIGHT])
 clock = pygame.time.Clock()
 start_time_movement = pygame.time.get_ticks()
 start_time_gravity = pygame.time.get_ticks()
 
-moving_tiles = [Entity(screen, tetrominos.create_instance("O"))]
-static_tiles = []
+# entity = tetrominos.create_instance("O", screen)
+# static_entities = []
+entities = []
+entities.append(tetrominos.create_instance("O", screen))
 elapsed_time_timeout = 600
 running = True
 while running:
@@ -100,50 +38,43 @@ while running:
             elapsed_time_timeout = 100
         else:
             elapsed_time_timeout = 600
-        for entity in moving_tiles:
-            for tile in entity.tiles:
-                tile.move_y(tile.height)
+        for tile in entity.tiles:
+            tile.move_y(tile.height)
 
     elapsed_time = pygame.time.get_ticks() - start_time_movement
     if elapsed_time >= 30:
         start_time_movement = pygame.time.get_ticks()
         if keys[pygame.K_RIGHT]:
-            for entity in moving_tiles:
+            for entity in [e for e in entities if not e.static]:
                 entity.move_x(entity.tiles[0].width)
         if keys[pygame.K_LEFT]:
-            for entity in moving_tiles:
+            for entity in [e for e in entities if not e.static]:
                 entity.move_x(-entity.tiles[0].width)
 
         at_bottom = False
-        for entity in moving_tiles:
+        for entity in [e for e in entities if not e.static]:
             for tile in entity.tiles:
                 if tile.y + tile.height >= screen.get_height():
                     at_bottom = True
                     break
                 else:
-                    for static_entity in static_tiles:
+                    for static_entity in [e for e in entities if e.static]:
                         for static_tile in static_entity.tiles:
                             if tile.y + tile.height >= static_tile.y and tile.x >= static_tile.x and tile.x + tile.width <= static_tile.x+static_tile.width:
                                 at_bottom = True
                                 break
                     if at_bottom:
                         break
-            if at_bottom:
-                break
         if at_bottom:
-            for entity in moving_tiles:
-                static_tiles.append(entity)
-            moving_tiles = [Entity(screen, tetrominos.create_instance("O"))]
+            [entity for entity in entities if not entity.static][0].static = True
+            entities.append(tetrominos.create_instance("O", screen))
 
     # drawing section
             
     # Fill the background with white
     screen.fill((50, 50, 50))
     
-    for entity in moving_tiles:
-        for tile in entity.tiles:
-            tile.draw()
-    for entity in static_tiles:
+    for entity in entities:
         for tile in entity.tiles:
             tile.draw()
             
--- tetrominos.py
+++ tetrominos.py
@@ -1,11 +1,16 @@
 import numpy as np
 
+import tile
 
 class Base:
-    def __init__(self, mask, cords, color):
+    def __init__(self, mask, cords, color, screen, symbol):
+        self.screen = screen
         self.mask = np.array(mask, dtype=int)
         self.cords = cords
         self.color = color
+        self.tiles = [tile.Tile(screen, 0,0,self.color), tile.Tile(screen, screen.get_width()/10,0, self.color)]
+        self.symbol = symbol
+        self.static = False
 
     def __str__(self):
         return np.array_str(self.mask)
@@ -22,63 +27,70 @@ class Base:
     def move_down(self):
         self.cords[1] += 1
 
+    def move_x(self, x):
+        for tile in self.tiles:
+            if not (tile.x + tile.width + x <= self.screen.get_width() and tile.x + x >= 0):
+                return
+        for tile in self.tiles:
+            tile.move_x(x)
+
 
 class I(Base):
-    def __init__(self, x = 0, y = 0):
-        super().__init__([[1, 1, 1, 1]], [x, y], (55, 255, 255))
+    def __init__(self, screen, x, y):
+        super().__init__([[1, 1, 1, 1]], [x, y], (55, 255, 255), screen, "I")
 
 
 class J(Base):
-    def __init__(self, x = 0, y = 0):
+    def __init__(self, screen, x, y):
         super().__init__([[1, 0, 0],
-                          [1, 1, 1]], [x, y], (51, 51, 255))
+                          [1, 1, 1]], [x, y], (51, 51, 255), screen, "J")
 
 
 class L(Base):
-    def __init__(self, x = 0, y = 0):
+    def __init__(self, screen, x, y):
         super().__init__([[0, 0, 1],
-                          [1, 1, 1]], [x, y], (255, 128, 0))
+                          [1, 1, 1]], [x, y], (255, 128, 0), screen, "L")
 
 
 class O(Base):
-    def __init__(self, x = 0, y = 0):
+    def __init__(self, screen, x, y):
         super().__init__([[1, 1],
-                          [1, 1]], [x, y], (255, 255, 51))
+                          [1, 1]], [x, y], (255, 255, 51), screen, "O")
 
 
 class S(Base):
-    def __init__(self, x = 0, y = 0):
+    def __init__(self, screen, x, y):
         super().__init__([[0, 1, 1],
-                          [1, 1, 0]], [x, y], (0, 255, 0))
+                          [1, 1, 0]], [x, y], (0, 255, 0), screen, "S")
 
 
 class T(Base):
-    def __init__(self, x = 0, y = 0):
+    def __init__(self, screen, x, y):
         super().__init__([[0, 1, 0],
-                          [1, 1, 1]], [x, y], (255, 0, 255))
+                          [1, 1, 1]], [x, y], (255, 0, 255), screen, "T")
 
 
 class Z(Base):
-    def __init__(self, x = 0, y = 0):
+    def __init__(self, screen, x, y):
         super().__init__([[1, 1, 0],
-                          [0, 1, 1]], [x, y], (255, 51, 51))
+                          [0, 1, 1]], [x, y], (255, 51, 51), screen, "Z")
 
 
-def create_instance(name, x=0, y=0):
+def create_instance(name, screen, x = 0, y = 0):
     name_upper = name.upper()
     if name_upper == "I":
-        return I(x, y)
+        return I(screen, x, y)
     elif name_upper == "J":
-        return J(x, y)
+        return J(screen, x, y)
     elif name_upper == "L":
-        return L(x, y)
+        return L(screen, x, y)
     elif name_upper == "O":
-        return O(x, y)
+        return O(screen, x, y)
     elif name_upper == "S":
-        return S(x, y)
+        return S(screen, x, y)
     elif name_upper == "T":
-        return T(x, y)
+        return T(screen, x, y)
     elif name_upper == "Z":
-        return Z(x, y)
+        return Z(screen, x, y)
     else:
         raise ValueError(f"{name} is not valid tetromino type")
