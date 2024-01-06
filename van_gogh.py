c907c74147c9158d69339b8f9fb21b71dea1bd21
madlsz <dlugosz00@gmail.com>
2024-01-06 17:09:13 +0100

    border and background colors customization

--- van_gogh.py
+++ van_gogh.py
@@ -2,9 +2,11 @@ import pygame
 import numpy as np
 
 class VanGogh():
-    def __init__(self, screen, border_thickness = 1):
+    def __init__(self, screen, border_thickness = 1, border_color = (0, 0, 0), background_color = (66, 66, 66)):
         self.screen = screen
         self.border_thickness = border_thickness
+        self.border_color = border_color
+        self.background_color = background_color
         self.color_map = {
             73: (55, 255, 255),
             74: (51, 51, 255),
@@ -33,10 +35,10 @@ class VanGogh():
     
 
     def draw(self, board):
-        self.screen.fill((66, 66, 66))
+        self.screen.fill(self.background_color)
         for (y, x), value in np.ndenumerate(board):
             if value != 0:
-                pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(x * self.tile_width, y * self.tile_height, self.tile_width, self.tile_height))
+                pygame.draw.rect(self.screen, self.border_color, pygame.Rect(x * self.tile_width, y * self.tile_height, self.tile_width, self.tile_height))
                 pygame.draw.rect(self.screen, self.color_map[value], pygame.Rect(x * self.tile_width - self.border_thickness, y * self.tile_height - self.border_thickness, self.tile_width - self.border_thickness, self.tile_height - self.border_thickness))
         pygame.display.update()
 
