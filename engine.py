790118ded19770afbcb5b9d57d3c8648b7abfd83
madlsz <dlugosz00@gmail.com>
2024-01-06 13:15:16 +0100

    better movement

--- engine.py
+++ engine.py
@@ -37,30 +37,34 @@ class Engine:
                     self.running = False
 
             keys = pygame.key.get_pressed()
+
+            # gravity
+            if keys[pygame.K_DOWN]:
+                self.gravity_time_timeout = 100
+            else:
+                self.gravity_time_timeout = 600
             elapsed_time = pygame.time.get_ticks() - self.gravity_time
             if elapsed_time >= self.gravity_time_timeout:
                 self.gravity_time = pygame.time.get_ticks()
-                if keys[pygame.K_DOWN]:
-                    self.gravity_time_timeout = 100
-                else:
-                    self.gravity_time_timeout = 600
                 self.game.clear_board()
                 self.game.move_tetromino_down()
 
-            elapsed_time = pygame.time.get_ticks() - self.movement_time
-            if elapsed_time >= self.movement_time_timeout:
-                self.movement_time = pygame.time.get_ticks()
-                if keys[pygame.K_RIGHT]:
+            # horizontal movement
+            if keys[pygame.K_RIGHT] or keys[pygame.K_LEFT]:
+                elapsed_time = pygame.time.get_ticks() - self.movement_time
+                if elapsed_time >= self.movement_time_timeout:
+                    self.movement_time = pygame.time.get_ticks()
                     self.game.clear_board()
-                    self.game.move_tetromino_right()
-                elif keys[pygame.K_LEFT]:
-                    self.game.clear_board()
-                    self.game.move_tetromino_left()
-
-            elapsed_time = pygame.time.get_ticks() - self.rotation_time
-            if elapsed_time >= self.rotation_time_timeout:
-                self.rotation_time = pygame.time.get_ticks()
-                if keys[pygame.K_UP]:
+                    if keys[pygame.K_LEFT]:
+                        self.game.move_tetromino_left()
+                    elif keys[pygame.K_RIGHT]:
+                        self.game.move_tetromino_right()
+            
+            # rotations
+            if keys[pygame.K_UP]:
+                elapsed_time = pygame.time.get_ticks() - self.rotation_time
+                if elapsed_time >= self.rotation_time_timeout:
+                    self.rotation_time = pygame.time.get_ticks()
                     self.game.clear_board()
                     self.game.rotate_tetromino()
 
