00a6e1fc77395ec9e1b6c5c0fe753cfe2dd2b2d1
madlsz <dlugosz00@gmail.com>
2024-01-13 22:25:03 +0100

    settings skeleton

--- game/scenes/settings.py
+++ game/scenes/settings.py
@@ -25,7 +25,30 @@ class SceneSettings(SceneBase):
                 self.switch_to_setter,
                 "menu",
                 id="return",
-            )
+            ),
+            Button(
+                200,
+                50,
+                "save changes",
+                self.switch_to_setter,
+                "menu",
+                id="save",
+            ),
+            Button(
+                200,
+                50,
+                "Settings",
+                None,
+                background_color=(0, 0, 0, 0),
+                id="settings",
+            ),
+            Button(
+                350,
+                50,
+                "Random pieces: True",
+                None,
+                id="random_pieces_toggle",
+            ),
         ]
 
     def process_input(self, events, keys_pressed):
@@ -34,7 +57,13 @@ class SceneSettings(SceneBase):
                 self.terminate()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 for button in self.buttons:
-                    button.click()
+                    if button.click():
+                        self.new_state = True
+                        if button.id == "random_pieces_toggle":
+                            if button.caption_str == "Random pieces: True":
+                                button.edit_caption("Random pieces: False")
+                            else:
+                                button.edit_caption("Random pieces: True")
 
     def update(self):
         pass
@@ -47,6 +76,14 @@ class SceneSettings(SceneBase):
                 if button.id == "return":
                     button.x = button.width * 0.05
                     button.y = self.screen.get_height() - button.height * 1.2
+                    self.screen.blit(button.surface, (button.x, button.y))
+                elif button.id == "save":
+                    button.x = self.screen.get_width() - button.width * 1.05
+                    button.y = self.screen.get_height() - button.height * 1.2
+                    self.screen.blit(button.surface, (button.x, button.y))
+                elif button.id == "settings":
+                    button.x = (self.screen.get_width() - button.width) // 2
+                    button.y = 10 * (i + 1)
                     self.screen.blit(button.surface, (button.x, button.y))
                 else:
                     button.x = (self.screen.get_width() - button.width) // 2
